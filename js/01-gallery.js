6e3e797229af4b078e63ceda993c0936e415b178
Volodymyr Aleshchenko <aleshchenko.v99@gmail.com>
2022-06-08 20:33:27 +0300

    task1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const galleryListItems = createGallery(galleryItems);
+
+function createGallery(collection) {
+  return collection
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+						<a class="gallery__link" href="${original}">
+							<img
+								class="gallery__image"
+								src="${preview}"
+								data-source="${original}"
+								alt="${description}"
+							/>
+						</a>
+					</div>`
+    )
+    .join("");
+}
+
+function addGalleryToPage(el) {
+  galleryEl.innerHTML = el;
+}
+
+addGalleryToPage(galleryListItems);
+
+galleryEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.nodeName === "IMG") {
+    const instance = basicLightbox.create(
+      `
+				<img class="create" src="${e.target.dataset.source}">
+			`
+    );
+    instance.show();
+  }
+});
