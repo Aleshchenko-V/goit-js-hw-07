a58899f1544261f1519da4ceba9d1280fb434e36
Volodymyr Aleshchenko <aleshchenko.v99@gmail.com>
2022-06-09 13:31:12 +0300

    task2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+const galleryListItems = createGallery(galleryItems);
+
+function createGallery(collection) {
+  return collection
+    .map(
+      ({ preview, original, description }) =>
+        `
+					<a class="gallery__item" href="${original}">
+						<img class="gallery__image" src="${preview}" alt="${description}" />
+					</a>
+				`
+    )
+    .join("");
+}
+
+function addGalleryToPage(el) {
+  galleryEl.insertAdjacentHTML("beforeend", el);
+}
+
+addGalleryToPage(galleryListItems);
+
+galleryEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  const isGalleryItem = e.target.classList.contains("gallery__image");
+  if (!isGalleryItem) {
+    return;
+  }
+
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionSelector: "img",
+    captionType: "attr",
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+});
